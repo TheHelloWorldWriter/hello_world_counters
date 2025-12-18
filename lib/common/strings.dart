9078a8803d860d242032c027926660dc3d3ebe15
TechAurelian <dev@techaurelian.com>
2025-12-18 13:44:25 +0200

    Update copyright comments and license references across multiple files

--- lib/common/strings.dart
+++ lib/common/strings.dart
@@ -1,7 +1,8 @@
 // Copyright 2020-2025 The Hello World Writer. All rights reserved.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://www.thehelloworldwriter.com/helloworldcounters/license/.
+// https://www.thehelloworldwriter.com
+//
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file.
 
 /// App wide UI string constants.
 library;
